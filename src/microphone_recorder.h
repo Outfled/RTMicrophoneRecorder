e1cc8e52aa8763753cead0674e2333c4d7a3b780
Outfled <55547083+Outfled@users.noreply.github.com>
2022-10-28 17:02:33 -0400

    Update microphone_recorder.h

--- src/microphone_recorder.h
+++ src/microphone_recorder.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #include <Windows.h>
-#include <mmsystem.h>
 
 /* Key to press to stop recording */
 #define STOP_REC_KEY    VK_ESCAPE
+
 
 DWORD RecordMicrophoneAudio(LPCWSTR lpszOutputFile);
